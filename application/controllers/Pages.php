d14cbceda5424f28dca0b3c77fa7a5c8e9555249
Xvareon <91329439+Xvareon@users.noreply.github.com>
2021-11-19 15:59:54 +0800

    Created Registration and Login Pages

---
+++ application/controllers/Pages.php
@@ -0,0 +1,18 @@
+<?php
+class Pages extends CI_Controller {
+
+    public function view($page = 'index')
+    {
+            if ( ! file_exists(APPPATH.'views/registration/'.$page.'.php'))
+            {
+                    echo "Whoops, we don't have a page for that!";
+                    show_404();
+            }
+
+            $data['title'] = ucfirst($page); // Capitalize the first letter
+
+            $this->load->view('templates/header', $data);
+            $this->load->view('registration/'.$page, $data);
+            $this->load->view('templates/footer', $data);
+    }
+}
---
+++ application/views/templates/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>style.css">
+</head>
+<body>
