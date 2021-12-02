11fdd5be73d4f75212e034162ae36637350eaccb
Gavs <poOdrag0n69@gmail.com>
2021-12-02 17:54:30 +0800

    Added home button

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/demo/';
+$config['base_url'] = 'http://localhost/Team-06-BSCS3CD/';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,6 +7,11 @@
         href = "<?php echo base_url(); ?>posts">
         Posts
     </a>
+    <br><br>
+    <a
+        href = "<?php echo base_url(); ?>">
+        Home
+    </a>
 
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>style.css">
 </head>
