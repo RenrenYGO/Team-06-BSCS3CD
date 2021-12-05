88d1a735f37c3e561269aad08622bc7d268439bf
Gavs <poOdrag0n69@gmail.com>
2021-12-06 01:34:24 +0800

    Updated application

--- application/views/registration/index.php
+++ application/views/registration/index.php
@@ -1,16 +1,3 @@
-<?php 
-  session_start(); 
-
-  if (!isset($_SESSION['name'])) {
-  	$_SESSION['msg'] = "You must log in first";
-  	header('location: login');
-  }
-  if (isset($_GET['logout'])) {
-  	session_destroy();
-  	unset($_SESSION['name']);
-  	header("location: login");
-  }
-?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -23,23 +10,7 @@
 	<h2>Home Page</h2>
 </div>
 <div class="content">
-  	<!-- notification message -->
-  	<?php if (isset($_SESSION['success'])) : ?>
-      <div class="error success" >
-      	<h3>
-          <?php 
-          	echo $_SESSION['success']; 
-          	unset($_SESSION['success']);
-          ?>
-      	</h3>
-      </div>
-  	<?php endif ?>
 
-    <!-- logged in user information -->
-    <?php  if (isset($_SESSION['name'])) : ?>
-    	<p>Welcome <strong><?php echo $_SESSION['name']; ?></strong></p>
-    	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
-    <?php endif ?>
 </div>
 		
 </body>
