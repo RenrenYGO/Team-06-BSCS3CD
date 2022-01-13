1b27cac2179bd8eec2f75cef5dbd369dbc7c8196
Gavs <poOdrag0n69@gmail.com>
2022-01-13 21:11:43 +0800

    Added Threads and updated some merging errors

---
+++ application/controllers/Forgot.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Forgot extends CI_Controller {
+    public function __construct(){
+	    parent::__construct();
+	}
+
+    public function index(){
+        $this->load->view('registration/forgot');
+        if ($_SERVER['REQUEST_METHOD']=='POST'){
+            $email = $this->input->post('email');      
+            $findemail = $this->forgot_model->forgotpassword($email);  
+            if($findemail){
+                $this->forgot_model->sendpassword($findemail);      
+            }else{ 
+                $this->session->set_flashdata('msg',' Email not found!');
+                redirect('login');     
+            }
+        }
+    }
+}
