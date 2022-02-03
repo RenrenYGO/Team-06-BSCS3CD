d63d415686bbc07fd7059d9ede569cd1ddf57d7f
pawnse <jponcemandap@gmail.com>
2022-02-04 00:58:12 +0800

    faqs changes

--- application/views/templates/faqs.php
+++ application/views/templates/faqs.php
@@ -13,23 +13,23 @@
 <h1 class="title p-5 text-center">Frequently Asked Questions</h1>
 <div class="container py-4">
     <h4>I cannot open my account, what should I do?</h4>
-    <p class="lead ms-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed velit ex, libero itaque dolorum totam. Odit voluptate amet dicta esse architecto porro nulla praesentium corrupti tempore, laboriosam ea numquam cupiditate?</p>
+    <p class="lead ms-3">Send us an email at Commhub@gmail.com to provide us the complete details.</p>
 </div>
 <div class="container py-4">
-    <h4>I cannot open my account, what should I do?</h4>
-    <p class="lead ms-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed velit ex, libero itaque dolorum totam. Odit voluptate amet dicta esse architecto porro nulla praesentium corrupti tempore, laboriosam ea numquam cupiditate?</p>
+    <h4>How can I change my password?</h4>
+    <p class="lead ms-3">Once you are logged In, click on your avatar on the upper right corner and choose "Account Settings"</p>
 </div>
 <div class="container py-4">
-    <h4>I cannot open my account, what should I do?</h4>
-    <p class="lead ms-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed velit ex, libero itaque dolorum totam. Odit voluptate amet dicta esse architecto porro nulla praesentium corrupti tempore, laboriosam ea numquam cupiditate?</p>
+    <h4>How can I send feedbacks</h4>
+    <p class="lead ms-3">Go to the help center which can be found at the bottom of the page. Click "Get Some Help"</p>
 </div>
 <div class="container py-4">
-    <h4>I cannot open my account, what should I do?</h4>
-    <p class="lead ms-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed velit ex, libero itaque dolorum totam. Odit voluptate amet dicta esse architecto porro nulla praesentium corrupti tempore, laboriosam ea numquam cupiditate?</p>
+    <h4>What is whatsauce?</h4>
+    <p class="lead ms-3">Whatsauce is the extension of Commhub, it is a blog technically about few topics.</p>
 </div>
 <div class="container py-4">
-    <h4>I cannot open my account, what should I do?</h4>
-    <p class="lead ms-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed velit ex, libero itaque dolorum totam. Odit voluptate amet dicta esse architecto porro nulla praesentium corrupti tempore, laboriosam ea numquam cupiditate?</p>
+    <h4>Can i use two tags at once?</h4>
+    <p class="lead ms-3">Unfortunately, we haven't implemented that yet. But updates are coming and that will include that too!</p>
 </div>
 
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js " integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p " crossorigin="anonymous "></script>
