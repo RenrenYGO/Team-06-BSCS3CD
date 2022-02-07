e303debe702a3215a32bd005dbd47291c2e67c41
mltario <yelatario@gmail.com>
2022-02-07 22:19:57 +0800

    added layout dev, posts dropdown

--- application/controllers/Popularity.php
+++ application/controllers/Popularity.php
@@ -8,7 +8,7 @@ class Popularity extends CI_Controller {
 	}
 
     public function posts(){
-        $data['title'] = "Most Popular Posts";
+        $data['title'] = "Top Posts";
         $data['threads'] = $this->threads_model->get_threads();
         $data['users'] = $this->user_model->get_users();
         $data['posts'] = $this->post_model->get_posts_by_popularity();
